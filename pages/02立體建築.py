4b57d6a2c5635b551513fb44e7c243c05bcf9146
deejing <deejing8964@gmail.com>
2025-11-11 13:25:40 +0000

    1111

--- pages/02立體建築.py
+++ pages/02立體建築.py
@@ -5,12 +5,16 @@ import leafmap.maplibregl as leafmap
 def create_map():
 
     m = leafmap.Map(
-        style="liberty",
-        projection="globe",
+        center=[-74.0095, 40.7046],
+        zoom=16,
+        pitch=60,
+        bearing=-17,
+        style="positron",
         height="750px",
-        zoom=2.5,
         sidebar_visible=True,
     )
+    m.add_basemap("Satellite", visible=False)
+    m.add_overture_3d_buildings(template="simple")
     return m
 
 
