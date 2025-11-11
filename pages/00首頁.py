a3968350c1e3ba024d88b360a471bed58a075ea4
deejing <deejing8964@gmail.com>
2025-11-11 13:11:33 +0000

    1111

--- pages/00首頁.py
+++ pages/00首頁.py
@@ -5,9 +5,9 @@ count = solara.reactive(0)
 def Page():
     with solara.Column(align="center"):
         solara.Title("首頁")
-        markdown = "歡迎使用我的Solara app！"
+        markdown = "## 歡迎使用我的Solara app！"
         solara.Markdown(markdown)
-        solara.Markdown(f"## 功德+{count.value}")
+        solara.Markdown(f"# 功德+{count.value}")
         def increment():
             count.value += 1 
         solara.Button("按我增加功德", on_click=increment)
